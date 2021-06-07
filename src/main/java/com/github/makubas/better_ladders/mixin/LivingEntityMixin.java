e2b0992c58a798e33275ece2d73839c4cf564ebf
Jakub Westa <jakub.westa@gmail.com>
2021-06-07 22:34:23 +0200

    Iron ladder pt. 2

--- src/main/java/com/github/makubas/better_ladders/mixin/LivingEntityMixin.java
+++ src/main/java/com/github/makubas/better_ladders/mixin/LivingEntityMixin.java
@@ -1,25 +1,27 @@
 package com.github.makubas.better_ladders.mixin;
 
+import com.github.makubas.better_ladders.block.IronLadderBlock;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
+
 
     public LivingEntityMixin(EntityType<?> type, World world) {
         super(type, world);
     }
 
-    @Inject(at = @At("TAIL"), method = "applyClimbingSpeed")
-    private void applyClimbingSpeed(Vec3d motion, CallbackInfoReturnable<Vec3d> cir) {
-
+    @Inject(at = @At("RETURN"), method = "applyClimbingSpeed")
+    private void applyClimbingSpeed(CallbackInfoReturnable<Boolean> cir) {
 
     }
+
 }
---
+++ src/main/java/com/github/makubas/better_ladders/mixin/PlayerEntityMixin.java
@@ -0,0 +1,28 @@
+package com.github.makubas.better_ladders.mixin;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+
+@Mixin(PlayerEntity.class)
+public abstract class PlayerEntityMixin extends LivingEntity {
+
+    protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Inject(at = @At("TAIL"), method = "tick")
+    public void tick(CallbackInfo ci) {
+        World world = getEntityWorld();
+        if (isClimbing()) {
+            System.out.println("Climbing");
+        }
+    }
+
+}
